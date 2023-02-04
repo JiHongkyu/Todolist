cc8ad0bf3333e3149fd3d406c4dc8007cbeeb3eb
JiHongkyu <121606131+JiHongkyu@users.noreply.github.com>
2023-02-04 14:53:15 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,54 +1,25 @@
-const todo_form = document.querySelector('.todo_form');
-const todo_input = document.querySelector('.todo_form input');
-const todo_ul = document.querySelector('.todo_list');
-let arrTodo = [];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="todo">
+        <div class="day">
+            <h1>0000년 00월 00일</h1>
+            <h2>O요일</h2>
+        </div>
+        <form class="todo_form">
+            <input type="text" placeholder="New Todo" required>
+        </form>
+        <ul class="todo_list"></ul>
+    </div>
 
-function saveTodo() {
-    localStorage.setItem('todos', JSON.stringify(arrTodo));
-}
-
-function deleteTodo(button) {
-    const removeLi = button.target.parentElement;
-    arrTodo = arrTodo.filter( (newTodo) => String(newTodo.id) !== removeLi.id)
-    saveTodo();
-    removeLi.remove();
-}
-
-function insertTodo(newTodo) {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const button = document.createElement('button');
-
-    li.appendChild(span);
-    li.appendChild(button);
-    li.id = newTodo.id;
-    span.innerText = newTodo.text;
-    button.innerText = "X";
-    todo_ul.appendChild(li);
-
-    button.addEventListener('click', deleteTodo);
-}
-
-function submitTodo(event) {
-    event.preventDefault();
-
-    const newTodo = {
-        text: todo_input.value,
-        id: Date.now(),
-    };
-
-    todo_input.value = '';
-    arrTodo.push(newTodo);
-    insertTodo(newTodo);
-    saveTodo();
-}
-
-todo_form.addEventListener('submit', submitTodo);
-const savedTodo = localStorage.getItem('todos');
-
-if(savedTodo !== null) {
-    const parsedTodo = JSON.parse(savedTodo);
-    
-    arrTodo = parsedTodo;
-    arrTodo.forEach(insertTodo);
-}
+    <script src="js/index.js"></script>
+    <script src="js/date.js"></script>
+</body>
+</html>
